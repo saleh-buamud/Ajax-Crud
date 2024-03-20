49bc93ecafaf122cdff3166c56045cdcbb110b81
Saleh buamud <buamudsaleh@gmail.com>
2024-03-20 22:16:38 +1400

    Route

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
 Route::get('/',[ProductController::class,'index'])->name('product.index');
 Route::Post('/add-product',[ProductController::class,'addProduct'])->name('product.addProduct');
 Route::get('about',function(){
-    return 'saleh';
+    return 'saleh-88cssc';
 });
